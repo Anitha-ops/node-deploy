46495bba614fae360cbac25b246e033c62bcfe34
Anitha-ops <75459401+Anitha-ops@users.noreply.github.com>
2020-12-04 13:11:25 +1100

    Update version_check.sh

--- version_check.sh
+++ version_check.sh
@@ -2,7 +2,7 @@
 RELEASE_HASH=`buildkite-agent meta-data get ndoe_app_version`
 
 while  [ `curl -s  -H "Content-Type: application/json" -d '{"query": "query {healthCheckContacts {version {commit}}}"}' \
-  https://$ENDPOINT/version | jq -r '.data.healthCheckNoodeapp.version.commit'` != $RELEASE_HASH ]; \
+  https://$ENDPOINT/version | jq -r '.data.healthCheckNodeapp.version.commit'` != $RELEASE_HASH ]; \
   do
     echo "Waiting for the new POD to start"
     sleep 2
